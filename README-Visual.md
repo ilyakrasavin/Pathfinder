a451aea3f9413848ebc982eab3b795d7861259e3
ikrasavi <ilyakrasavin@icloud.com>
2022-05-15 12:01:12 -0700

    Entity Factory Updates

--- README-Visual.md
+++ README-Visual.md
@@ -1,4 +1,4 @@
 
 Compiling on Mac OS (SFML installed via Homebrew)
 
-g++ main-visual.cpp -I /usr/local/Cellar/sfml/2.5.1_1/include -L /usr/local/Cellar/sfml/2.5.1_1/lib -lsfml-graphics -lsfml-window -lsfml-system
+g++ -std=c++11 main-visual.cpp -I /usr/local/Cellar/sfml/2.5.1_1/include -L /usr/local/Cellar/sfml/2.5.1_1/lib -lsfml-graphics -lsfml-window -lsfml-system
--- src/main-visual.cpp
+++ src/main-visual.cpp
@@ -1,42 +1,132 @@
-#include <SFML/Graphics.hpp>
 
 using namespace std;
 
+#include <SFML/Graphics.hpp>
+
 #include <iostream>
-// Setup (window, loop etc)
-// Application Loop (Event creation, update handling, display/render)
-// Cleanup
+#include <string>
+#include <vector>
+
+#include <memory>
 
 
-// EVENT-Driven programming
+// Base Interface for all components
+class IComponent{
+
+    public:
+
+        // WHY NOT pure virtual
+        virtual void render(sf::RenderWindow& ref){};
+        virtual void SetPosition(float x, float y){};
+
+        // Releases Base Resources
+        virtual ~IComponent(){};
+
+};
+
+
+class Sprite2DComponent : public IComponent{
+
+    public:
+
+        Sprite2DComponent(std::string filepath){
+            m_texture.loadFromFile(filepath);
+            m_sprite.setTexture(m_texture);
+        }
+
+        void SetPosition(float x, float y){
+            m_sprite.setPosition(x, y);
+        }
+
+        void render(sf::RenderWindow& ref) override{
+            ref.draw(m_sprite);
+        }
+
+
+
+        // Releases Derived Class Resources
+        ~Sprite2DComponent(){}
+        // i.e. 
+        // IComponent* b = new Sprite2DComponent
+        // delete b - makes virtual f. call to ~IComponent()
+        // Since it's virtual -> Calls ~Sprite2DComponent() -> ~IComponent()
+
+    private:
+        sf::Texture m_texture;
+        sf::Sprite m_sprite;
+
+};
+
+
+// Entitites such as Start / End Markers, Current / Searching Algo Positions 
+class GameEntity{
+    public:
+        GameEntity(string name){
+            n_name = name;
+        }
+
+        ~GameEntity(){}
+
+        float getXPos() const{return m_x;}
+        float getYPos() const{return m_y;}
+
+        void addComponent(string filename){
+            
+            shared_ptr<Sprite2DComponent> comp = make_shared<Sprite2DComponent>(filename);
+            m_components.push_back(comp);
+        }
+
+        // Render all drawable components
+        void RenderComponents(sf::RenderWindow& ref){
+
+            for(int i = 0; i < m_components.size(); i++){
+                m_components[i]->render(ref);
+            }
+
+        }
+
+        // Why setting positions for all entities?
+        void SetPosition(float x, float y){
+
+            for(int i = 0; i < m_components.size(); i++){
+
+                m_components[i]->SetPosition(x, y);
+
+            }
+
+        }
+
+
+    private:
+
+        float m_x, m_y; // Game Entity positions
+        string n_name; // Entity Name
+        vector< shared_ptr<IComponent> > m_components; // Embedded Components
+
+};
+
+
+class Board{
+
+};
+
+
 
 int main()
 {
-
-
-    // Main Board Image
-    sf::Texture board;
-    board.loadFromFile("../assets-static/tiles.jpg");
-
-    // Void Sprite Structure taking a texture
-    sf::Sprite mySprite(board);
-
-    // Scale the Structure
-    mySprite.setScale(3.0f, 2.0f);
-
     // Main Rendering Window 
-    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1200, 1200), "Main Window!");
+    window.setFramerateLimit(60);
 
-    // Shape Examples
-    sf::CircleShape shape(100.f);
-    sf::RectangleShape secondShape(sf::Vector2f(180,180));
 
-    secondShape.setPosition(sf::Vector2f(780, 780));
-    secondShape.setFillColor(sf::Color::Red);
+    Sprite2DComponent board("../assets-static/tiles.jpg");
 
-    shape.setFillColor(sf::Color::Magenta);
+    sf::RectangleShape cell(sf::Vector2f(60,60));
+    cell.setPosition(sf::Vector2f(0, 0));
+    cell.setFillColor(sf::Color::Black);
+    cell.move(60, 60);
 
-    
+ 
     // Set up fonts
     sf::Font font;
     if (!font.loadFromFile("../assets-static/Raleway-Regular.ttf"))
@@ -51,7 +141,17 @@ int main()
 
     // Frame Rate Setup
     sf::Clock clock;
-    window.setFramerateLimit(60);
+
+
+    vector<GameEntity> entities;
+
+    for(int i = 0; i < 50; i++){
+        GameEntity test("ghost" + to_string(i));
+        test.addComponent("../assets-static/ghost.jpg");
+        test.SetPosition(400 + i, 700 + i);
+        entities.push_back(test);
+    }
+
 
     // Rendering Window Outer loop
     while (window.isOpen())
@@ -76,58 +176,31 @@ int main()
                 window.close();
 
             if(event.type == sf::Event::MouseButtonPressed){
-                shape.move(3, 6);
-                secondShape.move(-3, -6);
+                cout<<sf::Mouse::getPosition().x<<endl;
+                cout<<sf::Mouse::getPosition().y<<endl;
             }
 
-            if(event.type == sf::Event::KeyReleased){
-                cout<<"Key was pressed"<<endl;
-                if(event.key.code == sf::Keyboard::D){
-                    cout<<"KEY D!"<<endl;
-                }
-            }
-
-            mouseReleased = false;
-
-            if(event.type == sf::Event::MouseButtonReleased){
-                mouseReleased = true;
-                cout<<"Mouse button released"<<endl;
-            }
-
-            if(sf::Mouse::isButtonPressed(sf::Mouse::Button::Left) && mouseReleased){
-                cout<<"Left mouse button pressed!"<<endl;
-            }
-
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Z)){
-
-                cout<<"Z pressed"<<endl;
-            }
-
-            if(sf::Mouse::isButtonPressed(sf::Mouse::Button::Left) && mouseReleased){
-                cout<<"Left mouse button pressed!"<<endl;
-            }
-
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Z)){
-
-                cout<<"KOK"<<endl;
-            }
-
-
+            
             // Rendering
-
             window.clear();
-            window.draw(mySprite);
-            window.draw(shape);
+            board.render(window);
+
+            // Draw all sprites
+            for(int i = 0; i < entities.size(); i++){
+                entities[i].RenderComponents(window);
+            }
+
             window.draw(text);
-            window.draw(secondShape);
+            window.draw(cell);
             window.display();
 
         }
 
         // No Rendering / Display Updates for the actions done outside of Polling Loop
         // Main App updates HERE
-    
+        // Algortihm Updates / Checks etc...
+
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
