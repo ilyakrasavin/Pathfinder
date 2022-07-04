78ef08ac9e7274712db44107f50e1fd4e65b2bd1
Ilia Krasavin <ilyakrasavin@icloud.com>
2022-07-04 12:34:13 -0700

    Neighbour Origin calculations (Not Functional)

--- src/Algorithms/A-Star.cpp
+++ src/Algorithms/A-Star.cpp
@@ -21,6 +21,14 @@ struct CompareFScore {
 };
 
 
+int computeDirection(shared_ptr<NodeExp> currentNodeRef, int neighbourIdx){
+
+    // Compute the neighbour direction
+    // Return corrent distance
+
+}
+
+
 bool AStar(Application* appControls){
 
     shared_ptr<NodeExp> startNode = appControls->getWGraphRef()->getStartNode();
@@ -43,10 +51,15 @@ bool AStar(Application* appControls){
     // Used to traceBack the path
     vector<exlporedAStar> traceback;
 
+    int indexIncrements = 20;
+
+
     for(int i = 0; i < appControls->getWGraphRef()->getMatrixSize(); i++){
+
+        exlporedAStar newExplored;
+
         exploredNodesIdx.push_back(0);
         openNodesIdx.push_back(0);
-        exlporedAStar newExplored;
         traceback.push_back(newExplored);
 
 		// TODO: MARK HEURISTIC ESTIMATES
@@ -81,8 +94,6 @@ bool AStar(Application* appControls){
 
         vector<shared_ptr<NodeExp>> neighbourNodes;
 
-        cout<<(currentNode->getNextRight() == nullptr)<<endl;
-
         neighbourNodes.push_back(currentNode->getNextRight());
         neighbourNodes.push_back(currentNode->getNext45up());
         neighbourNodes.push_back(currentNode->getNextUp());
@@ -107,16 +118,65 @@ bool AStar(Application* appControls){
                 continue;
             }   
 
-			///////////////////////////////////////
-			// Figure how the neighbour is reached!
-			// TODO
-			///////////////////////////////////////
+            int neighbourIdx = neighbour->getIdx();
 
             // If not in Open List
             // Add to open list
             // Make current the parent
             // Record G, H, F of the square
-            else if(openNodesIdx[neighbour->getIdx()] == 0){
+            bool isTopLeft = neighbourIdx % indexIncrements == 0 && (neighbourIdx == 0);
+            bool isTopEdge = (neighbourIdx > 0) && (neighbourIdx < (indexIncrements - 1));
+            bool isTopRight = (neighbourIdx % indexIncrements == (indexIncrements - 1)) && (neighbourIdx == ((indexIncrements - 1)));
+            bool isLeftEdge = (neighbourIdx % indexIncrements == 0) && ((neighbourIdx + indexIncrements) < appControls->getWGraphRef()->getMatrixSize());
+            bool isRightEdge = (neighbourIdx % indexIncrements == (indexIncrements - 1)) && ((neighbourIdx + indexIncrements) < appControls->getWGraphRef()->getMatrixSize());
+            bool isBottomLeft = (neighbourIdx % indexIncrements == 0) && ((neighbourIdx + indexIncrements) > appControls->getWGraphRef()->getMatrixSize());
+            bool isBottomEdge = (neighbourIdx > (appControls->getWGraphRef()->getMatrixSize() - (indexIncrements + 1))) && (neighbourIdx < (appControls->getWGraphRef()->getMatrixSize() - 1));
+            bool isBottomRight = (neighbourIdx % indexIncrements == (indexIncrements - 1)) && (neighbourIdx + 1 >= appControls->getWGraphRef()->getMatrixSize());
+            bool isMiddle = {!(isTopLeft || isTopEdge || isTopRight || isLeftEdge 
+                            || isRightEdge || isBottomLeft || isBottomEdge || isBottomRight)};
+
+            // // Node is NextRight and Node is not a Right Top/Bottom/Edge Node
+            // if((neighbourIdx == (currentNode->getIdx() + 1)) && !(isTopRight || isRightEdge || isBottomRight)){
+            //     fullDistance = smallestDistanceNode->distanceTo + appControls->getWGraphRef()->nodeMatrix.at(smallestDistanceNode->boardIdx)->getNextRightDistance();
+            // }
+
+            // // Node is NEXT45UP and is not a Left Top / Top Edge /  OR Right Top/Edge/Bottom node 
+            // else if((neighbourIdx == (smallestDistanceNode->boardIdx - indexIncrements + 1)) && !(isTopLeft || isTopEdge || isTopRight || isRightEdge || isBottomRight)){
+            //     fullDistance = smallestDistanceNode->distanceTo + appControls->getWGraphRef()->nodeMatrix.at(smallestDistanceNode->boardIdx)->getNext45UpDistance();
+            // }
+
+            // // Node is UP and is not Left Top / Top Edge / Right Top node
+            // else if((neighbourIdx == (smallestDistanceNode->boardIdx - indexIncrements)) && !(isTopLeft || isTopRight || isTopEdge)){
+            //     fullDistance = smallestDistanceNode->distanceTo + appControls->getWGraphRef()->nodeMatrix.at(smallestDistanceNode->boardIdx)->getNextUpDistance();
+            // }
+
+            // // Node is PREV45UP and is not Left Top/Bottom/Edge or Right Top
+            // else if((neighbourIdx == (smallestDistanceNode->boardIdx - indexIncrements - 1)) && !(isTopLeft || isBottomLeft || isLeftEdge || isTopRight)){
+            //     fullDistance = smallestDistanceNode->distanceTo + appControls->getWGraphRef()->nodeMatrix.at(smallestDistanceNode->boardIdx)->getPrev45UpDistance();
+            // }
+
+            // // Node is PrevLeft and is not Left Top/Bottom/Edge
+            // else if((neighbourIdx == (smallestDistanceNode->boardIdx - 1)) && !(isLeftEdge || isTopLeft || isBottomLeft)){
+            //     fullDistance = smallestDistanceNode->distanceTo + appControls->getWGraphRef()->nodeMatrix.at(smallestDistanceNode->boardIdx)->getPrevLeftDistance();
+            // }
+
+            // // Node is PREV45Down and is not Right Bottom or Left Top/Edge/Bottom or Bottom Edge
+            // else if((neighbourIdx == (smallestDistanceNode->boardIdx + indexIncrements - 1)) && !(isBottomRight || isTopLeft || isLeftEdge || isBottomLeft || isBottomEdge)){
+            //     fullDistance = smallestDistanceNode->distanceTo + appControls->getWGraphRef()->nodeMatrix.at(smallestDistanceNode->boardIdx)->getPrev45DownDistance();
+            // }
+
+            // // Node is Down and is not a bottom edge / left/right
+            // else if((neighbourIdx == (smallestDistanceNode->boardIdx + indexIncrements)) && !(isBottomEdge || isBottomLeft || isBottomRight)){
+            //     fullDistance = smallestDistanceNode->distanceTo + appControls->getWGraphRef()->nodeMatrix.at(smallestDistanceNode->boardIdx)->getNextDownDistance();
+            // }
+
+            // // Node is Next45Down and is not Right Top/Edge/Botom or Bottom Edge/Left/Right
+            // else if((neighbourIdx == (smallestDistanceNode->boardIdx + indexIncrements + 1)) && !(isTopRight || isRightEdge || isBottomRight || isBottomEdge || isBottomLeft || isBottomRight)){
+            //     fullDistance = smallestDistanceNode->distanceTo + appControls->getWGraphRef()->nodeMatrix.at(smallestDistanceNode->boardIdx)->getNext45DownDistance();
+            // }
+
+
+            if(openNodesIdx[neighbour->getIdx()] == 0){
 
                 cout<<"Neighbour Not in open list. Inserting"<<endl;
 
@@ -131,6 +191,7 @@ bool AStar(Application* appControls){
 
                 neighbour->getCellRef()->setExplored();
                 neighbour->getCellRef()->setTexture("../assets-static/node-neighbour.jpg");
+                
 				// TODO: ADD Correct traversal costs to heuristic estimations
                 neighbour->getCellRef()->setScore(traceback[neighbour->getIdx()].fScore, neighbour->getIdx());
 
